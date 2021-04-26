a5ae00b61bd662e1b41cd0134b299041e4e6cb3c
slitaz-cn <77770064+slitaz-cn@users.noreply.github.com>
2021-04-26 13:42:50 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,25 +2,23 @@
 
 cat << EOF > /etc/xray/config.json
 {
-    "inbounds": [
-        {
-            "port": $PORT,
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "$UUID",
-                        "level": 0
-                    }
-                ],
-                 "decryption": "none"
-            },
-            "streamSettings": {
-                "network": "ws",
-                "security": "none"
-            }
-        }
-    ],
+	"inbounds": [{
+		"port": $PORT,
+		"listen": "0.0.0.0",
+		"protocol": "vless",
+		"settings": {
+			"clients": [{
+				"id": "$UUID"
+			}],
+			"decryption": "none"
+		},
+		"streamSettings": {
+			"network": "ws",
+			"wsSettings": {
+				"path": "$WSPATH"
+			}
+		}
+	}],
 	"outbounds": [{
 		"protocol": "freedom"
 	}]
